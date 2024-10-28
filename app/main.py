586e1eb23a4916f5044ffec4faa89d65a88399d3
tianshi04 <zomodat123@gmail.com>
2024-10-28 18:32:38 +0700

    add fastapi

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
+import time
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+    "http://localhost",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+@app.get("/")
+async def root():
+    return {"mesage": "Hello World"}
